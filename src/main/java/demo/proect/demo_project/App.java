6e2fde7f91ae63ceeec8d4161674915df7fe284e
Paranidharan Devaraj <mailtodharan@gmail.com>
2019-04-05 20:50:54 -0700

    init updates

---
+++ src/main/java/demo/proect/demo_project/App.java
@@ -0,0 +1,13 @@
+package demo.proect.demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
